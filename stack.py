58c55d9cc483d3ab18ba921b13afb4222eade590
Yashu_King11 <89150253+yashu1wwww@users.noreply.github.com>
2022-04-21 10:43:56 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -4,12 +4,10 @@ driver = webdriver.Chrome()
 driver.get("https://stackoverflow.com/users/login")
 
 input=driver.find_element_by_xpath('//*[@id="email"]')
-input.send_keys('yashwanth6675@gmail.com')
+input.send_keys('virat6670@gmail.com')
 
 input=driver.find_element_by_xpath('//*[@id="password"]')
-input.send_keys('Yash2@#$%')
+input.send_keys('viratkohli')
 
 input=driver.find_element_by_xpath('//*[@id="submit-button"]')
 input.click()
-
-
